2d49b434cd648dcb5c66d80d080984cfc4f8293e
Moinul Islam <moinulislam159632@gmail.com>
2024-12-06 00:46:19 +0600

    Marry

---
+++ script.js
@@ -0,0 +1,31 @@
+function clickNo() {
+    let no = document.querySelector('.no');
+    let yes = document.querySelector('.yes');
+
+    no.classList.add('yes-temp');
+    yes.classList.add('no-temp');
+
+    no.classList.remove('no');
+    yes.classList.remove('yes');
+
+    no.classList.add('yes');
+    yes.classList.add('no');
+}
+
+function clickYes() {
+    let no = document.querySelector('.no');
+    let yes = document.querySelector('.yes');
+    let qn = document.querySelector('.qn');
+    let msg = document.querySelector('.msg');
+    let body = document.body;
+    
+    yes.style.display = "none";
+    no.style.display = "none";
+    qn.style.display = "none";
+
+    body.style.backgroundColor = "#8fb5d6";
+    msg.innerHTML = "I knew you'd say YES! ❤️";
+    msg.style.fontSize = "5rem";
+    msg.style.transition = "color 0.5s";
+
+}
